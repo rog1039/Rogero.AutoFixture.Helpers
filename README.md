d1dddefcff9852dfa2166adfb2cf2ecb130bb7ab
Paul Rogero <paulrogero@gmail.com>
2016-02-11 07:20:33 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Rogero.AutoFixture.Helpers
+
+This is a simple library of methods to help configure Moq mock objects using AutoFixture.
+
+I typically use it like the following:
+
+```csharp
+var _fixture = new Fixture();
+
+fixture.GetMock<IWatchDefinitionRepository>()
+		.Setup(x => x.GetWatchDefinitions())
+		.Returns(testCase.WatchDefinitions);
+		
+fixture.GetMock<IWatchDefinitionRepository>()
+		.Setup(x => x.GetMostRecentWatchRunRecords())
+		.Returns(testCase.TableWatchRecordRuns);
+```
+
+The helper method GetMock<T> performs two functions:
+* Creates a Moq mock object
+* Registeres the mock and the mocked object with the _fixture Fixture variable.
+
+This way we can continue to call GetMock<T>() to retrieve the mock to mock out additional methods.
+
+When we resolve from the container for the mocked object or an object that uses the mocked object, the _fixture will supply the mocked object.
+
+## Helpful Hint
+
+This project works best when creating fixture like this: ```var fixture = new Fixture().Customize(new AutoMoqCustomization());```
+
+This way we can resolve from the Fixture and any dependencies required will be Moq'ed automatically.
+
+The ```AutoMoqCustomization``` object is contained in another Nuget package, ```AutoFixture.AutoMoq``` available here: https://www.nuget.org/packages/AutoFixture.AutoMoq
---
+++ src/Rogero.AutoFixture.Helpers/Rogero.AutoFixture.Helpers/AutoFixtureMockExtensionMethods.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq.Expressions;
+using Moq;
+using Moq.Language.Flow;
+using Ploeh.AutoFixture;
+
+namespace Rogero.AutoFixture.Helpers
+{
+    public static class AutoFixtureMockExtensionMethods
+    {
+        public static void Mock<T>(this IFixture fixture, Action<Mock<T>> moqAction) where T : class
+        {
+            var moq = fixture.GetMock<T>();
+            MockAction(fixture, moq, moqAction);
+        }
+
+        static void MockAction<T>(this IFixture fixture, Mock<T> moq, Action<Mock<T>> moqAction) where T : class
+        {
+            moqAction(moq);
+            T subject = moq.Object;
+            fixture.Register(() => subject);
+            fixture.Register(() => moq);
+        }
+
+        public static Mock<T> GetMock<T>(this IFixture fixture) where T : class
+        {
+            var moq = fixture.Create<Mock<T>>();
+            fixture.Register(() => moq);
+            return moq;
+        }
+
+        public static ISetup<T> Setup<T>(this IFixture fixture, Expression<Action<T>> expression) where T : class
+        {
+            var mock = fixture.GetMock<T>();
+            return mock.Setup(expression);
+        }
+
+        public static ISetup<T, TResult> Setup2<T, TResult>(this IFixture fixture, Expression<Func<T, TResult>> expression) where T : class
+        {
+            var mock = fixture.GetMock<T>();
+            return mock.Setup(expression);
+        }
+
+        public static void Verify<T>(this IFixture fixture, Expression<Action<T>> action, Times times) where T : class
+        {
+            fixture.GetMock<T>().Verify(action, times);
+        }
+    }
+}
