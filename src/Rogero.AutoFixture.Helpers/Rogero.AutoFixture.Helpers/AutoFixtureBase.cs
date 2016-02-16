b4827732c09996eb525cf53d9a5117b10edda242
Paul Rogero <paulrogero@gmail.com>
2016-02-16 10:54:38 -0500

    Added autofixture test base class.

---
+++ src/Rogero.AutoFixture.Helpers/Rogero.AutoFixture.Helpers/AutoFixtureBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+
+namespace Rogero.AutoFixture.Helpers
+{
+    public class AutoFixtureBase
+    {
+        protected readonly IFixture _fixture;
+
+        public AutoFixtureBase()
+        {
+            _fixture = new Fixture();
+            _fixture.Customize(new AutoConfiguredMoqCustomization());
+        }
+    }
+}
